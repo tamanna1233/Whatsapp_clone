0d3c9c21fcb7216cede4135784c4eac4f2b139a9
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2025-02-22 20:24:07 +0530

    add messge delete feature

--- backend/src/controller/status.controller.js
+++ backend/src/controller/status.controller.js
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
+import { Chat } from "../Models/chat.model.js";
 import { Status } from "../Models/status.model.js";
+import { User } from "../Models/usermodel.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -15,7 +18,9 @@ const uploadStatus = asyncHandler(async(req,res)=>{
      throw new apiError(400,"Failed to upload status");
     }
 const status =  await Status.create({content: {url:response.url , public_id:response.public_id}, text})
-console.log(response)
+await User.findByIdAndUpdate(req.user._id,{
+    $push:{statuses:status._id}
+})
  res .status(200) .json (new apiResponse(201,status,"Status uploaded successfully"));
 })
 
@@ -51,4 +56,56 @@ await deleteOnCloudninary(content.content.public_id)
     return res.status(200).json(new apiResponse(200, 'Status deleted successfully'));
 });
 
-export {uploadStatus,deleteStatus}
+
+const getAllStatus=asyncHandler(async(req,res)=>{
+
+  const id=req.user._id 
+  if(!id){
+    throw new apiError(400,"unathorised user")
+  }
+const Status= await Chat.aggregate([
+    {$match:{participants:new mongoose.Types.ObjectId(id) }},
+    {$unwind:"$participants"},
+    {$match:{participants: {$ne :new mongoose.Types.ObjectId(id)}}},
+    {$group:{_id:"$participants"}},
+    {$lookup:{from:"users",localField:"_id",foreignField:"_id",as:"userInfo"},
+    pipeline: [
+        {
+          $lookup: {
+            from: "status",
+            localField: "Statuses", // Assuming "Statuses" is an array of ObjectIds
+            foreignField: "_id",
+            as: "statusinfo"
+          }
+        },
+        { $unwind: { path: "$statusinfo", preserveNullAndEmptyArrays: true } }, // Unwind status array safely
+        {
+          $project: {
+            _id: 1,
+            content: "$statusinfo.content.url", // Extracting content URL
+            text: "$statusinfo.text" // Extracting text
+          }
+        }
+      ]},
+    {$unwind:"$userInfo"},
+    
+    {$project:{_id:1,name:"$userInfo.name",status:"$userInfo.statusinfo"}}
+])
+
+if(!Status){
+    throw new apiError(500,"status not found")
+}
+return res.status(200).json(new apiResponse(200,Status,"status is fetch succesfullly"))
+
+
+
+})
+
+const getmystatus=asyncHandler(async(req,res)=>{
+    const id =req.user._id
+    if(!id){
+        throw new apiError(400,"user id is required")
+    }
+})
+
+export {uploadStatus,deleteStatus,getAllStatus}
--- backend/src/router/status.routes.js
+++ backend/src/router/status.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyjwt } from "../middleware/auth.middleware.js";
-import { deleteStatus, uploadStatus } from "../controller/status.controller.js";
+import { deleteStatus, getAllStatus, uploadStatus } from "../controller/status.controller.js";
 import { upload } from "../middleware/mullter.middleware.js";
 
 const router = Router();
 router.route('/uploadStatus').post(verifyjwt,upload.single('content') ,uploadStatus)
 router.route('/deleteStatus').delete(verifyjwt,deleteStatus)
+router.route("/getallstatus").get(verifyjwt,getAllStatus)
 export default router;
