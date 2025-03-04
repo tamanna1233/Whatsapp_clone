b860544369a92cf7715890b9abd67d1647b02a95
TAMANNA <tamannakarthol320@gmail.com>
2025-03-04 12:09:10 +0530

    add

--- backend/src/router/status.routes.js
+++ backend/src/router/status.routes.js
@@ -9,16 +9,9 @@ import {
 import { upload } from '../middleware/mullter.middleware.js';
 
 const router = Router();
-<<<<<<< HEAD
 router.route('/uploadStatus').post(verifyjwt,upload.single('content') ,uploadStatus)
 router.route('/deletestatus/:statusId').delete(verifyjwt,deleteStatus)
 router.route("/getallstatus").get(verifyjwt,getAllStatus)
 router.route("/getmystatus").get(verifyjwt,getmystatus)
-=======
-router.route('/uploadStatus').post(verifyjwt, upload.single('content'), uploadStatus);
-router.route('/deleteStatus').delete(verifyjwt, deleteStatus);
-router.route('/getallstatus').get(verifyjwt, getAllStatus);
-router.route('/getmystatus').get(verifyjwt, getmystatus);
->>>>>>> 5d759b8e9ae707086410d3d8ec94f0f83f0f4f39
 
 export default router;
